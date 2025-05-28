f933585c37d4f696bc70bb750d3f1da745dd1006
milenaosc <milenadigitalsosc@gmail.com>
2025-05-28 20:01:30 +0200

    made the GetFortune function

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import "fmt"
 
+func GetFortune(){
+	fmt.Println("Things are looking auspicious today")
+}
 
 func main(){
 	fmt.Println("Hello from main, lets get started")
+	GetFortune()
 }
